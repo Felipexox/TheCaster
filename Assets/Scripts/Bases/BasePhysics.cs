6af6318bd9b0ba6f1aa7576f9f2fafb09cd10b69
Felipexox <felipeaa100@gmail.com>
2018-05-01 10:53:50 -0300

    [Added]   - movement is now compatible with take a external force

--- Assets/Scripts/Bases/BasePhysics.cs
+++ Assets/Scripts/Bases/BasePhysics.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class BasePhysics : MonoBehaviour {
 	protected Rigidbody rigid;
+	protected Vector3 acceleration;
+	protected Vector3 velocity;
 	protected virtual void Awake(){
 		rigid = GetComponent<Rigidbody> ();
 	}
-	protected virtual void Move (Vector3 direction, float velocity){ 
-		Vector3 currentVelocity = rigid.velocity;
-		Vector3 directionVelocity = direction.normalized;
-
-		currentVelocity += directionVelocity;
-		currentVelocity = Vector3.ClampMagnitude (currentVelocity, velocity);
-		rigid.velocity = currentVelocity;
+	protected virtual void Move (Vector3 p_Direction, float p_Velocity){ 
+		Vector3 acceleration = ResultForce (p_Direction, p_Velocity);
+		Vector3 resultVelocity = VelocityController (acceleration);
+		rigid.velocity = resultVelocity;
 	}
-
+	protected virtual Vector3 ResultForce(Vector3 p_Direction, float p_Force){
+		acceleration += (p_Direction * p_Force) * Time.deltaTime;
+		//friction
+		acceleration -= acceleration * Time.deltaTime * 10;
+		return acceleration;
+	}
+	protected virtual Vector3 VelocityController(Vector3 p_Acceleration){
+		velocity += p_Acceleration;
+		//friction
+		velocity -= velocity * Time.deltaTime * 5;
+		return velocity;
+	}
+	public virtual void AddForce(Vector3 p_Direction, float p_Force){
+		ResultForce (p_Direction, p_Force);
+	}
 	protected virtual void TakeHit(BaseAbility ability){ }
 
 
